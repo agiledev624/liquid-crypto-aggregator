2c92f2cc6dec1f30406a77641d23a8aafe9fda06
agiledev624 <agiledev624@gmail.com>
2022-04-14 13:21:45 +0800

    default changed

--- src/network.js
+++ src/network.js
@@ -89,7 +89,7 @@ export const allNetworks = [
 const network = allNetworks.find(n => window.location.hash.startsWith('#' + n.hash));
 
 if (!network) {
-  window.location.hash = allNetworks[0].hash;
+  window.location.hash = allNetworks[1].hash;
   window.location.reload();
 } else {
   window.REACT_APP_NETWORK_ID = network.id;
