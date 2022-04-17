095baab5a16169b8e335d1af902490184ff33da3
agiledev624 <agiledev624@gmail.com>
2022-04-17 23:20:21 +0800

    ec2 backend

--- src/features/helpers/getApiInfo.js
+++ src/features/helpers/getApiInfo.js
@@ -1,1 +1,2 @@
-export const apiUrl = process.env.REACT_APP_API_URL || 'https://liquidc-backend.herokuapp.com';
+// export const apiUrl = process.env.REACT_APP_API_URL || 'https://liquidc-backend.herokuapp.com';
+export const apiUrl = process.env.REACT_APP_API_URL || 'https://api.liquid-crypto.io';
